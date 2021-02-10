5d944b586faa908d33048e2084b333a39a0a70a3
Albertalandia <75736179+Albertalandia@users.noreply.github.com>
2021-02-10 15:57:27 -0300

    Segunda entrega

--- script.js
+++ script.js
@@ -1,47 +1,80 @@
-var nombre = prompt("¿Cuál es tu nombre?");
-var dinero =  parseInt(prompt("¿Cuánto dinero necesitás?"));
-var meses = parseInt(prompt("¿En cuántos meses querés devolverlo?"));
-var intereses = parseInt((dinero * 5)/100);
 
-function cuotaPrestamo(prestamo) {
-    return prestamo.dinero + " " + prestamo.intereses
-}
-
-function prestamo (nombre, dinero, meses, intereses) {
-    this.nombre = nombre;
-    this.dinero = dinero;
-    this.meses = meses;
-    this.intereses = intereses;
-
-    this.informacion =  function(){
-        return cuotaPrestamo(this)
-    };
-}
-
-var miPrestamo = new prestamo (nombre, dinero, meses, intereses);
-
-
-var descuento = 100;
-var miCuotaConDescuento;
-var cuotaPrestamo = (dinero / meses) + intereses;
-var cliente;
-while (cliente !== "si" && cliente !== "sí" && cliente !== "no") {
-cliente = (prompt("¿sos cliente de nuestro banco?"));
-};
-
-function sacarCuotaFinal(){
+    var todosLosPrestamos = [
+        {
+            nombre: "Lucas",
+            dinero: 50000, 
+            meses: 12, 
+            intereses: parseInt((50000 * 5)/100),
+        }, {
+            nombre: "Daniela",
+            dinero: 100000, 
+            meses: 10, 
+            intereses: parseInt(10000 * 5)/100,
+        },
+            {
+            nombre: "Lisandro",
+            dinero: 60000, 
+            meses: 12, 
+            intereses: parseInt((60000 * 5)/100),
+        }
+    ]; 
     
-        if (cliente === "si") {
-            miCuotaConDescuento = alert("Tu cuota será de" + " " + "$" + (cuotaPrestamo - descuento));
-    }   else if (cliente === "no"){
-        alert("Tu cuota será de" + " " + "$" + cuotaPrestamo);
-    }   else {
-        (prompt("¿sos cliente de nuestro banco?"));
-    }   return miCuotaConDescuento;
+    function recogerDatos() {
 
+    var nombre = document.getElementById("nombre").value;
+    var dinero =  parseInt(document.getElementById("dinero").value);
+    var meses = parseInt(document.getElementById("meses").value);
+
+
+    var cliente = "";
+    var clienteSi = document.getElementById("si");
+    var clienteNo = document.getElementById("no");
+
+    if (clienteSi.checked){
+        cliente = clienteSi.value;
+    } else {
+        cliente = clienteNo.value;
+    }
+
+    var intereses = parseInt((dinero * 5)/100);
+
+    function infoCuotaPrestamo(prestamo) {
+        return prestamo.dinero + " " + prestamo.intereses
+    }
+
+    function prestamo (nombre, dinero, meses, intereses) {
+
+        this.nombre = nombre;
+        this.dinero = dinero;
+        this.meses = meses;
+        this.intereses = intereses;
+
+        this.informacion = function(){
+            return infoCuotaPrestamo(this)
+        };
+    }
+
+    var miPrestamo = new prestamo (nombre, dinero, meses, intereses);
+    todosLosPrestamos.push(new prestamo(nombre, dinero, meses, intereses));
+    sessionStorage.setItem("JSON", JSON.stringify( todosLosPrestamos))
+
+    console.log(todosLosPrestamos)
+
+    var descuento = 1000;
+    var cuotaPrestamo = (dinero / meses) + intereses;
+    var miCuotaConDescuento= cuotaPrestamo - descuento;
+    function sacarCuotaFinal(){
+        
+            if ( cliente ==="si") {
+                miCuotaConDescuento;
+        }   else if (cliente === "no"){
+            cuotaPrestamo;
+        }   return miCuotaConDescuento;
+    }
+
+
+    var parrafo = document.createElement("p");
+    var contenido = document.createTextNode("Me tenes que dar esto por mes: " + sacarCuotaFinal()); 
+    parrafo.appendChild(contenido);
+    document.body.appendChild(parrafo);
 }
-
-sacarCuotaFinal()
-
-
-
